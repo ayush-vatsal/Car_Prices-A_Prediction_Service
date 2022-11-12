09fdee9039778f651a07f43a48895aa9238dacd3
Ayush Vatsal <57457066+Ayush863@users.noreply.github.com>
2022-11-12 23:56:22 +0530

    Update run-feature-and-inference-pipelines.sh

--- scripts/run-feature-and-inference-pipelines.sh
+++ scripts/run-feature-and-inference-pipelines.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-cd src/01-module
-
 jupyter nbconvert --to notebook --execute Car_price_Feature_Pipeline.ipynb
 jupyter nbconvert --to notebook --execute Car_Price_Batch_Inference_Pipeline.ipynb
