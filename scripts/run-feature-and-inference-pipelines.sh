f1ba113a4b7490e4e73de15a4a9b05e1942bb919
Ayush Vatsal <ayushvatsal04@gmail.com>
2022-11-13 08:42:24 +0530

    adding files

--- scripts/run-feature-and-inference-pipelines.sh
+++ scripts/run-feature-and-inference-pipelines.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+!/bin/bash
 
 set -e
-
-cd src/01-module
 
 jupyter nbconvert --to notebook --execute Car_price_Feature_Pipeline.ipynb
 jupyter nbconvert --to notebook --execute Car_Price_Batch_Inference_Pipeline.ipynb
